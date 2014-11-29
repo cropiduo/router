71100d9c67d09511aa9173c8002f7be34fa81e37
cropiduo <urban.bartosz@gmail.com>
2014-11-29 17:54:46 +0100

    Added MongoDB configuration and fetching data from DB.

--- src/main/java/ab/testing/router/domain/UserGroup.java
+++ src/main/java/ab/testing/router/domain/UserGroup.java
@@ -1,7 +1,12 @@
 package ab.testing.router.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "usergroups")
 public class UserGroup {
 
+    @Id
     private String userId;
     private String group;
 
--- src/main/java/ab/testing/router/repository/impl/UserGroupRepositoryImpl.java
+++ src/main/java/ab/testing/router/repository/impl/UserGroupRepositoryImpl.java
@@ -2,13 +2,25 @@ package ab.testing.router.repository.impl;
 
 import ab.testing.router.domain.UserGroup;
 import ab.testing.router.repository.UserGroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
 
 @Repository
 public class UserGroupRepositoryImpl implements UserGroupRepository {
 
+    private MongoTemplate mongoTemplate;
+
+    @Autowired
+    public UserGroupRepositoryImpl(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
     @Override
     public UserGroup getUserGroupByUserId(String userId) {
-        return new UserGroup(userId, "fake_group");
+        return mongoTemplate.findOne(query(where("userId").is(userId)), UserGroup.class);
     }
 }
